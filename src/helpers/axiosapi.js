03d75e0aa9598627b91601e68355baa0bde55e85
mahirminhajk <kmahirminhaj@gmail.com>
2026-03-11 15:13:23 +0530

    update axios baseURL to production endpoint

--- src/helpers/axiosapi.js
+++ src/helpers/axiosapi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosapi = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://auto-finance-backend.vercel.app/api',
     withCredentials: true,
 });
 

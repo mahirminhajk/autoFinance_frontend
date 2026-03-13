6bbdadb8179bfb6f6ec16127519391480fd789cf
mahirminhajk <kmahirminhaj@gmail.com>
2026-03-13 12:14:07 +0530

    fix: update axios baseURL to use the production API endpoint

--- src/helpers/axiosapi.js
+++ src/helpers/axiosapi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosapi = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: 'https://autofinanceapi.kabsdigital.com/api',
     withCredentials: true,
 });
 //fix1
